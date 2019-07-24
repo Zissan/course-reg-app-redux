d13671c535be5d5be4356f3abc02cecbe175e03d
zissan <zissan.chowdhury@yahoo.com>
2019-07-24 23:42:29 +0530

    Created the Redux boiler plate, connected with the Container Component using mapStateToProps and mapDispatchToProps. Tried various mapDispatchToProps like ignorie it, manual, bindActionCreators and Object

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
 import "./index.css";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="jumbotron">
-        <h1 className="display-4">Course Registration App | React Redux</h1>
-        <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-        <hr className="my-4" />
-        <p>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
-        </p>
-        <a className="btn btn-primary btn-lg" role="button">
-          Learn more
-        </a>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById("app"));
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+const store = configureStore();
+render(
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
+  document.getElementById("app")
+);
